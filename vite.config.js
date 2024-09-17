1fa0a6e3031ab01925837250ccffabbca9f2fbb1
Yash <yashsharma786a@gmail.com>
2024-09-17 09:28:08 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'reaact3',
+  base: '/reaact3/',  // Add the trailing slash here
 })
+
